9b5ec856d651f9567cb4af4d3c3e0ab1f67b23de
“Martin” <mlatini50@gmail.com>
2023-04-20 12:13:28 +0200

    js

--- main.js
+++ main.js
@@ -96,7 +96,7 @@ do {
    }
 } while (randomNum !== userNum);
 
-*/
+
 
 document.getElementById("texto").innerHTML = "Hola, mundo!";
 
@@ -119,3 +119,241 @@ for (let i = 0; i < elementos.length; i++) {
     elementos[i].style.backgroundColor = "purple";
     
 }
+
+
+
+let arr = ["uno", 2, true, null, 5];
+
+for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+}
+
+
+let arr2 = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+let suma = 0
+
+
+for (let j = 0; j < arr2.length; j++) {
+    suma += arr2[j];
+}
+
+console.log(suma);
+
+
+
+let arr3 = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+]
+
+for (let i = 0; i < arr3.length; i++) {
+    for (let j = 0; j < arr3[i].length; j++) {
+       console.log(arr3[i][j]);
+    }
+}
+
+
+
+const array = [2, 4, 6, 8];
+let numMayor = array[0];
+let numMenor = array[0];
+
+function num() {
+    for (let i = 0; i < array.length; i++) {
+        if (numMayor > array[i]) {
+            numMayor = array[i]
+        }
+
+        if (numMenor < array[i]) {
+            numMenor = array[i]
+        }
+    }
+    console.log("mayor: " + numMayor + "," + "menor: " + numMenor)
+}
+
+num()
+
+
+
+function eliminarDuplicados(array) {
+    const array2 = [];
+
+   for (let i = 0; i < array.length; i++) {
+    const elementoActual = array[i];
+
+    if (!array2.includes(elementoActual)) {
+        array2.push(elementoActual);
+    }
+   }
+
+   return array2;
+}
+
+// console.log(eliminarDuplicados(["hola", "adios", "hola", "gracias"]))
+
+const numbers = [10, 20, 30, 40, 50, 60, 70, 80, 90];
+
+let numbers2 = numbers.slice(2,5);
+
+console.log(numbers2);
+
+// 2
+
+const names = ["Alice", "Bob", "Charlie", "David", "Eve"];
+console.log("el array original es:", names);
+let names2 = names.slice(1,3);
+names.splice(1, 2);
+
+console.log("los elementos eliminados son:", names2, "y el array que queda es", names);
+
+//3
+
+const letters = ["a", "b", "c", "d", "e", "f", "g"];
+letters.splice(3, 2, "x", "y");
+
+console.log(letters);
+
+//4
+
+const numbers3 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+numbers3.copyWithin(3, 0, 2);
+
+console.log(numbers3);
+
+
+//5
+
+const words = ["apple", "banana", "cherry", "date", "fig", "grape"];
+
+words.fill("ok", 1, 4);
+
+console.log(words);
+
+//6
+
+const numbers4 = [1, 2, 3, 7, 8, 9];
+let numbers5 = [4, 5, 6];
+
+let nuevoArray = numbers4.slice(0,3)
+let nuevoArray2 = numbers4.slice(3)
+
+let arrayDefinitivo = nuevoArray.concat(numbers5, nuevoArray2);
+
+console.log(arrayDefinitivo);
+
+
+
+// 1
+
+ let array = [2, 3, 9]
+
+
+
+const arrayModificado = array.map(function arreglo(potencia) {
+    return Math.pow(potencia, 3);
+})
+
+ console.log(arrayModificado);
+
+
+// 2
+
+const array2 = [1,2,3,4,5,6]
+
+const pares = array2.filter(function (numeros) {
+    return numeros % 2 === 0;
+})
+    
+    
+console.log(pares);
+
+
+
+
+const numerosArray = [0, -1, 4, -5]
+
+const numerosNegativos = numerosArray.some(function (numero) {
+    return numero < 0;
+});
+
+console.log(numerosNegativos);
+
+
+
+const palabras = ["hola", "chau", "gracias"]
+
+const tresCaracteres = palabras.every(function (elemento) {
+    return elemento.length >= 3
+})
+
+console.log(tresCaracteres);
+
+
+
+const obj = [
+    {persona: "Luis", edad: 18}, 
+    {persona: "Jose", edad: 34},
+    {persona: "Mario", edad: 22}];
+
+const masDe = obj.findIndex(function (elemento) {
+    return elemento.edad > 30
+})
+
+console.log(masDe);
+
+
+function square(x) {
+    return x * x;
+}
+
+const function1 = x => x * x;
+
+console.log(square(3), function1(3));
+
+function greet(name) {
+    return `hello, ${name}!`;
+}
+
+const function2 = name => `hello, ${name}!`;
+
+function sum(a, b, c) {
+    return a + b + c;
+}
+
+const function3 = (a, b, c) => a + b + c;
+
+
+const people = [
+    { name: 'Alice', age: 30 },
+    { name: 'Bob', age: 25 },
+    { name: 'Eve', age: 35 },
+    { name: 'Charlie', age: 28 }
+    ];
+
+const ordenar = (a, b) => {if (a.name > b.name) {
+    return 1 
+} else if (a.name < b.name){
+    return -1
+} else {
+    return 0
+}
+};
+
+const obj = people.sort(ordenar)
+
+console.log(obj);
+*/
+
+const cart = [
+    { product: 'Apple', price: 1, quantity: 3 },
+    { product: 'Banana', price: 2, quantity: 2 },
+    { product: 'Orange', price: 1.5, quantity: 4 }
+    ];
+
+const sumaTotal = cart.reduce((accumulator, currentValue) => {
+    return accumulator + (currentValue.price * currentValue.quantity);
+}, 0);
+
+console.log(sumaTotal);
